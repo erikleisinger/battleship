2a987bde60b6804aed7c4b4c32b9d37069e445ca
erikleisinger <erikleisinger@gmail.com>
2020-09-25 20:02:22 +0000

    refactored library to merge loadGame and battleship.js -- resolved functionality issues running in chrome browser

--- loadGame.js
+++ loadGame.js
@@ -1,3 +1,149 @@
+const hover = function (id) {
+  if (gameData.gameMode === 'place') {
+    checkBounds(id).then((arrayOfTiles) => {
+      console.log(arrayOfTiles)
+      return checkArray(arrayOfTiles);
+    }).then((array) => {
+        highlightTiles(array);
+    }).catch((error) => {
+      console.log(error);
+    });
+  } else if (gameData.gameMode === 'play') {
+  }
+};
+
+const unHover = function (id) {
+  if (gameData.gameMode === 'place') {
+    highlightNoMore()
+  };
+}
+
+// const { gameTiles, gameData, gameAssets } = require('./data');
+
+const boatPlacement = function() {
+  let currentArray = gameData.placement.currentSpot;
+  for (let item of currentArray) {
+    console.log(item);
+    let tile = document.getElementById(item);
+    console.log(tile);
+    tile.style.backgroundColor = gameAssets.colors[gameData.checkFor];
+    gameTiles[item].role = 'target';
+  }
+  let shipClass = gameData.checkFor;
+  gameData.checkFor = 'void';
+  gameData.placement[shipClass] = currentArray;
+  
+}
+
+const move = function (id) {
+  if (gameData.gameMode === 'place') {
+    boatPlacement()
+  } else if (gameData.gameMode === 'play') {
+    let data = gameTiles[id];
+    let tile = document.getElementById(id);
+    console.log(gameData.state);
+    let newColor = gameAssets.colors[gameData.state];
+    tile.style.backgroundColor = newColor;
+  }
+};
+
+
+
+
+const shipPlacementButton = function (cla) {
+  console.log('hi there');
+  console.log(gameData.placement[cla]);
+  if (gameData.placement[cla] !== 'n') {
+    
+    let tileArray = gameData.placement[cla];
+    for (let item of tileArray) {
+      let tile = document.getElementById(item);
+      tile.style.backgroundColor = 'white';
+    }
+    gameData.checkFor = cla;
+  } else {
+    gameData.checkFor = cla;
+  }
+};
+
+
+
+const checkBounds = function (idee) {
+  return new Promise((resolve, reject) => {
+    let id = Number(idee);
+    let axis = gameData.placement.axis;
+    let length = gameAssets.shipData[gameData.checkFor];
+    let newArray = [];
+    
+
+    if (axis === 'horz') {
+      for (let x = id; x < id + length; x++) {
+        
+        if ((x - 9) % 10 === 0 && x !== id + (length - 1)) {
+          console.log('array is out of bounds')
+        resolve(false);
+      }
+    }
+    
+    for (let x = id; x < id + length; x++) {
+    newArray.push(x);
+    }
+    } else if (axis === 'vert') {
+      for (let x = id; x < id + (length * 10); x += 10) {
+        console.log(x);
+        if (x - 90 > 0 && x !== (id + length * 10) - 1) {
+          console.log('array is out of bounds')
+        resolve(false);
+      }
+    }
+   
+    for (let x = id; x < id + (length * 10); x += 10) {
+      newArray.push(x);
+    }
+  }
+  resolve(newArray);
+  });
+};
+
+const checkArray = function (array) {
+  return new Promise((resolve, reject) => {
+    for (let item of array) {
+      console.log(gameTiles[item].role);
+      if (gameTiles[item].role === 'target') {
+        resolve(false);
+      }
+    }
+    gameData.placement.currentSpot = array;
+    resolve(array);
+  })
+  
+};
+
+let currentArray = '';
+
+const highlightTiles = function(array) {
+  currentArray = array;
+  let color = gameAssets.colors[gameData.checkFor];
+  for (let item of array) {
+    let tileToHighlight = document.getElementById(item);
+    tileToHighlight.style.backgroundColor = color;
+  }
+};
+
+const highlightNoMore = function() {
+  let array = currentArray;
+  for (let item of array) {
+    if (gameTiles[item].role === 'target') {
+    } else {
+      let tileToHighlight = document.getElementById(item);
+      tileToHighlight.style.backgroundColor = 'white';
+    }
+    
+  }
+};
+
+
+
 const createGrid = function () {
   {
     let board = document.getElementsByClassName("board")[0];
@@ -110,3 +256,5 @@ const setButtons = function () {
 
 };
 setButtons();
+
+
