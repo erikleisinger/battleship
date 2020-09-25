6e59c7840155e8e4086b11bb447f708cd3913583
erikleisinger <erikleisinger@gmail.com>
2020-09-25 19:36:15 +0000

    created hover() function, bound to evenListener mouseover which utilizes a chain of promises: checkBounds(): determines the length of the boat being placed (gameAssets.shipData) and determines if the location is outside board bounds checkArray(): checks the gameTiles information for each tile to make sure a boat isn't already assigned (gameData.a1.role === 'target' highLightTile() highlights each tile

--- battleship.js
+++ battleship.js
@@ -134,3 +134,79 @@ const setButtons = function () {
 };
 setButtons();
 
+const checkBounds = function (idee) {
+  return new Promise((resolve, reject) => {
+    let id = Number(idee);
+    let axis = gameData.placement.axis;
+    let length = gameAssets.shipData[gameData.checkFor];
+    let newArray = [];
+    
+
+    if (axis === 'horz') {
+      for (let x = id; x < id + length; x++) {
+        
+        if ((x - 9) % 10 === 0 && x !== id + (length - 1)) {
+          console.log('array is out of bounds')
+        resolve(false);
+      }
+    }
+    
+    for (let x = id; x < id + length; x++) {
+    newArray.push(x);
+    }
+    } else if (axis === 'vert') {
+      for (let x = id; x < id + (length * 10); x += 10) {
+        console.log(x);
+        if (x - 90 > 0 && x !== (id + length * 10) - 1) {
+          console.log('array is out of bounds')
+        resolve(false);
+      }
+    }
+   
+    for (let x = id; x < id + (length * 10); x += 10) {
+      newArray.push(x);
+    }
+  }
+  resolve(newArray);
+  });
+};
+
+const checkArray = function (array) {
+  return new Promise((resolve, reject) => {
+    for (let item of array) {
+      console.log(gameTiles[item].role);
+      if (gameTiles[item].role === 'target') {
+        resolve(false);
+      }
+    }
+    gameData.placement.currentSpot = array;
+    resolve(array);
+  })
+  
+};
+
+let currentArray = '';
+
+const highlightTiles = function(array) {
+  currentArray = array;
+  let color = gameAssets.colors[gameData.checkFor];
+  for (let item of array) {
+    let tileToHighlight = document.getElementById(item);
+    tileToHighlight.style.backgroundColor = color;
+  }
+};
+
+const hover = function (id) {
+  if (gameData.gameMode === 'place') {
+    checkBounds(id).then((arrayOfTiles) => {
+      console.log(arrayOfTiles)
+      return checkArray(arrayOfTiles);
+    }).then((array) => {
+        highlightTiles(array);
+    }).catch((error) => {
+      console.log(error);
+    });
+  } else if (gameData.gameMode === 'play') {
+  }
+};
+
