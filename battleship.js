b0e1de59da5e7f80a967292fb31f9d499618179d
erikleisinger <erikleisinger@gmail.com>
2020-09-25 19:30:41 +0000

    added buttons in battleship.html and a function setButtons() which applies functions to each which modifies values in the gameData object library.

--- battleship.js
+++ battleship.js
@@ -37,3 +37,68 @@ const generateGameTiles = function () {
 };
 generateGameTiles();
 
+const setButtons = function () {
+  let targButton = document.getElementsByClassName('target')[0];
+  targButton.addEventListener('click', function () {
+    gameData.state = 'target';
+    console.log(gameData);
+  });
+  let voidButton = document.getElementsByClassName('void')[0];
+  voidButton.addEventListener('click', function () {
+    gameData.state = 'void';
+    console.log(gameData);
+  });
+  let triedButton = document.getElementsByClassName('tried')[0];
+  triedButton.addEventListener('click', function () {
+    gameData.state = 'tried';
+    console.log(gameData);
+  });
+  let carrierButton = document.getElementsByClassName('carrier')[0];
+  carrierButton.addEventListener('click', function () {
+    gameData.checkFor = 'carrier';
+    shipPlacementButton('carrier');
+  });
+  let battleshipButton = document.getElementsByClassName('battleship')[0];
+  battleshipButton.addEventListener('click', function () {
+    gameData.checkFor = 'battleship';
+    shipPlacementButton('battleship');
+  });
+  let cruiserButton = document.getElementsByClassName('cruiser')[0];
+  cruiserButton.addEventListener('click', function () {
+    gameData.checkFor = 'cruiser';
+    shipPlacementButton('cruiser');
+  });
+  let submarineButton = document.getElementsByClassName('submarine')[0];
+  submarineButton.addEventListener('click', function () {
+    gameData.checkFor = 'submarine';
+    shipPlacementButton('submarine');
+  });
+  let destroyerButton = document.getElementsByClassName('destroyer')[0];
+  destroyerButton.addEventListener('click', function () {
+    shipPlacementButton('destroyer');
+    console.log(gameData);
+  });
+  let horzButton = document.getElementsByClassName('horz')[0];
+  horzButton.addEventListener('click', function () {
+    gameData.placement.axis = 'horz';
+    console.log(gameData);
+  });
+  let vertButton = document.getElementsByClassName('vert')[0];
+  vertButton.addEventListener('click', function () {
+    gameData.placement.axis = 'vert';
+    console.log(gameData);
+  });
+  let playButton = document.getElementsByClassName('play')[0];
+  playButton.addEventListener('click', function () {
+    gameData.gameMode = 'play';
+    console.log(gameData);
+  });
+  let placeButton = document.getElementsByClassName('place')[0];
+  placeButton.addEventListener('click', function () {
+    gameData.gameMode = 'place';
+    console.log(gameData);
+  });
+
+};
+setButtons();
+
