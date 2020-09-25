85ea364f6029c795ec76c54d6e9e337cfb46cdfd
erikleisinger <erikleisinger@gmail.com>
2020-09-25 19:47:58 +0000

    refactored library (part 1): created data.js for objects dealing with game data: - gameTiles - gameAssets - gameData

---
+++ data.js
@@ -0,0 +1,42 @@
+const gameTiles = {
+
+};
+const gameData = {
+  state: 'void',
+  gameMode: 'place',
+  checkFor: 'battleship',
+  placement: {
+    axis: 'horz',
+    currentSpot: [],
+    carrier: 'n',
+    battleship: 'n',
+    cruiser: 'n',
+    submarine: 'n',
+    destroyer: 'n'
+}
+};
+const gameAssets = {
+  colors: {
+    target: 'red',
+    void: 'white',
+    tried: 'black',
+    carrier: 'red',
+    battleship: 'orange',
+    cruiser: 'yellow',
+    submarine: 'green',
+    destroyer: 'blue'
+  },
+    shipData: {
+      carrier: 5,
+      battleship: 4,
+      cruiser: 3,
+      submarine: 3,
+      destroyer: 2
+    }
+};
+
+module.exports {
+  gameTiles,
+  gameData,
+  gameAssets
+};
